971439ef77f3b13cce63a6f4b66271ea68c9ffa2
Ryan Ramsey <rramsey1115@gmail.com>
2025-01-29 13:10:57 -0600

    updated formatting

--- server/routes/items.js
+++ server/routes/items.js
@@ -29,72 +29,65 @@ const items = (app) => {
       }
     }) // end of post request
 
-    .get(async (req,res) => {
+    .get(async (req, res) => {
       try {
         const items = await Item.find({});
-        if(!items) {
+        if (!items) {
           res.json({});
           return;
         }
-        const formattedData = items.map((item) =>{
+        const formattedData = items.map((item) => {
           return {
             _id: item.id,
             name: item.name,
             details: item.details,
-            amount: item.amount
-          }
+            amount: item.amount,
+          };
         });
         res.status(200).json(formattedData);
         return;
-      } catch (err){
-        console.log("error getting items:", err)
-        res.send("error getting items")
+      } catch (err) {
+        console.log("error getting items:", err);
+        res.send("error getting items");
       }
     }); // end of get request
 
-    app
-      .route("/api/items/delete")
-      .delete(async (req, res)=> {
-        try {
-          await Item.deleteMany({})
+  app.route("/api/items/delete").delete(async (req, res) => {
+    try {
+      await Item.deleteMany({});
 
-          res.status(200).json({
-            message: "All items have been deleted successfully"
-          })
-        } catch (err) {
-          console.log(`Error deleting all items: `, err)
-          res.status(500).send("Error deleting items");
-        }
-
+      res.status(200).json({
+        message: "All items have been deleted successfully",
       });
-      
-      app
-        .route("/api/items/delete/:id")
-        .delete(async (req, res) => {
+    } catch (err) {
+      console.log(`Error deleting all items: `, err);
+      res.status(500).send("Error deleting items");
+    }
+  });
 
-          const {id} = req.params;
+  app.route("/api/items/delete/:id").delete(async (req, res) => {
+    const { id } = req.params;
 
-          if(!id) {
-            console.log("No id found")
-          }
+    if (!id) {
+      console.log("No id found");
+    }
 
-          try {
-            const itemToDelete = await Item.findByIdAndDelete(id);
+    try {
+      const itemToDelete = await Item.findByIdAndDelete(id);
 
-            if(!itemToDelete) {
-              return res.status(404).json({message: "Item not found"})
-            }
+      if (!itemToDelete) {
+        return res.status(404).json({ message: "Item not found" });
+      }
 
-            res.status(200).json({
-              message: `Item with id ${id} has been deleted`,
-              item: itemToDelete
-            })
-
-          } catch (err) {
-            console.log("Error deleting by id", err);
-            res.status(500).send("Error deleting item by id")
-          }
-      });// end of delete request
+      res.status(200).json({
+        message: `Item with id ${id} has been deleted`,
+        item: itemToDelete,
+      });
+    } catch (err) {
+      console.log("Error deleting by id", err);
+      res.status(500).send("Error deleting item by id");
+    }
+  }); // end of delete request
 };
 
 export default items;
